1000c6e8a702a42931e5d5ef3ec8a3e5d6dc5df4
Mateus Koenig <mateuskoenig1@gmail.com>
2020-07-14 02:58:03 -0300

    SQL Adicionado, CRUD do Departamento Criado

---
+++ HoraExtraWebApMVC/Data/HoraExtraWebApMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HoraExtraWebApMVC.Models.Departamento;
+
+namespace HoraExtraWebApMVC.Data
+{
+    public class HoraExtraWebApMVCContext : DbContext
+    {
+        public HoraExtraWebApMVCContext (DbContextOptions<HoraExtraWebApMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HoraExtraWebApMVC.Models.Departamento.Departamento> Departamento { get; set; }
+    }
+}
---
+++ HoraExtraWebApMVC/Models/Departamento/Departamento.cs
@@ -0,0 +1,10 @@
+﻿namespace HoraExtraWebApMVC.Models.Departamento
+{
+    public class Departamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Gestor { get; set; }
+
+    }
+}
